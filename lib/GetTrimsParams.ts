48886c2dd0580247b3de8aee60ce59168a1fa9c1
JT Wheeler <jwheeler@JTs-MacBook-Pro.local>
2018-03-30 22:01:10 -0400

    getTrims accept enginePosition

--- lib/GetTrimsParams.ts
+++ lib/GetTrimsParams.ts
@@ -4,6 +4,7 @@ export interface GetTrimsParams {
     bodyStyle?: BodyStyle,
     doors?: number;
     drive?: string;
+    enginePosition?: string;
     year?: number;
     make?: string;
     model?: string;
